12dd861dbc3f6531a65cbfd0e984a5996685c006
Sebastien Deschamps <wwwbrtswww@gmail.com>
2018-07-21 16:36:13 +0100

    Try to customize /src/main/resources/i18n path for localised resources files

--- src/main/java/com/dsbackit/weblab/InternationalizationConfig.java
+++ src/main/java/com/dsbackit/weblab/InternationalizationConfig.java
@@ -2,8 +2,10 @@ package com.dsbackit.weblab;
 
 import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
@@ -11,6 +13,17 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class InternationalizationConfig extends WebMvcConfigurerAdapter {
+	private static final String[] BASENAMES = {
+		"i18n/messages.properties",
+		"i18n/messages_en_US.properties",
+		"i18n/messages_fr_FR.properties",
+	};
+    @Bean
+	 public MessageSource messageSource() {
+		 ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		 messageSource.setBasenames(BASENAMES);
+		 return messageSource;
+	 }
 	 @Bean
 	 public LocaleChangeInterceptor localeChangeInterceptor() {
 		 LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
