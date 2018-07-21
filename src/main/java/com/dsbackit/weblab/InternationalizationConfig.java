a61463a129716eb714e7a374f9ceeee6807303a5
Sebastien Deschamps <wwwbrtswww@gmail.com>
2018-07-21 16:18:05 +0100

    Fixed: Thymeleaf placeholders; remove localized resource configuration; and move localized resources files from src/main/resources/i18n to /src/main/resources

--- src/main/java/com/dsbackit/weblab/InternationalizationConfig.java
+++ src/main/java/com/dsbackit/weblab/InternationalizationConfig.java
@@ -2,10 +2,8 @@ package com.dsbackit.weblab;
 
 import java.util.Locale;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
@@ -13,22 +11,11 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class InternationalizationConfig extends WebMvcConfigurerAdapter {
-	private static final String[] BASENAMES = {
-		"i18n/messages.properties",
-		"i18n/messages_en_GB.properties",
-		"i18n/messages_fr_FR.properties",
-	};
-    @Bean
-	 public MessageSource messageSource() {
-		 ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		 messageSource.setBasenames(BASENAMES);
-		 return messageSource;
-	 }
 	 @Bean
 	 public LocaleChangeInterceptor localeChangeInterceptor() {
 		 LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
 		 localeChangeInterceptor.setParamName("lang");
-		 localeChangeInterceptor.setIgnoreInvalidLocale(true);
+		 localeChangeInterceptor.setIgnoreInvalidLocale(false);
 		 return localeChangeInterceptor;
 	 }
 	 @Override
@@ -40,11 +27,6 @@ public class InternationalizationConfig extends WebMvcConfigurerAdapter {
 		 CookieLocaleResolver localeResolver = new CookieLocaleResolver();
 		 localeResolver.setCookieName("lang");
 		 localeResolver.setDefaultLocale(Locale.UK);
-		 System.out.println("--------------");
-		 System.out.println("--------------");
-		 System.out.println(Locale.UK);
-		 System.out.println("--------------");
-		 System.out.println("--------------");
 		 return localeResolver;
 	 }
 }
